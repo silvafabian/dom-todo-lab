2e30d6afa0f8056948e446ae4bb4c836439f34b9
Fabian Silva <silvafabian74@yahoo.com>
2022-02-02 17:55:48 -0500

    Create cache element references

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const inpElement = document.getElementById('inp')
+// console.log(inpElement)
+const btnElement = document.getElementById('submit-button')
+// console.log(btnElement)
+const ulElement = document.getElementById('todo-list')
+// console.log(ulElement)
+
