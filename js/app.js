bf2b9fd220065dd77cc1abf39a6f8230823610b1
Fabian Silva <silvafabian74@yahoo.com>
2022-02-02 19:37:29 -0500

    solve bonus 2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 const inpElement = document.getElementById('inp')
-const ulElement = document.getElementById('todo-list')
+const ulElement  = document.getElementById('todo-list')
 const btnElement = document.getElementById('submit-button')
+const rstElement = document.getElementById('reset-button')
 
 btnElement.addEventListener('click', function(evt){
-  
-    if (inpElement.value !== '') {
+  if (inpElement.value !== '') {
     const newLi = document.createElement('li');
     newLi.textContent = inpElement.value;
     document.querySelector('ul').appendChild(newLi);
     inpElement.value = ''
     inpElement.focus()
   } 
+});
+
+// rstElement.addEventListener('click', function(evt){
+
+// })
+
+ulElement.addEventListener('click', function(evt){
+  evt.target.parentNode.removeChild(evt.target)
 })
