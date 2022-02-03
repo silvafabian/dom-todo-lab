3fb1760738d9ff4cac81d44455e2f4a8eb75f557
Fabian Silva <silvafabian74@yahoo.com>
2022-02-02 19:58:17 -0500

    solve bonus 1

--- js/app.js
+++ js/app.js
@@ -3,20 +3,27 @@ const ulElement  = document.getElementById('todo-list')
 const btnElement = document.getElementById('submit-button')
 const rstElement = document.getElementById('reset-button')
 
+let lisArr = []
+
 btnElement.addEventListener('click', function(evt){
   if (inpElement.value !== '') {
     const newLi = document.createElement('li');
     newLi.textContent = inpElement.value;
+    lisArr.push(newLi)
     document.querySelector('ul').appendChild(newLi);
     inpElement.value = ''
     inpElement.focus()
   } 
 });
 
-// rstElement.addEventListener('click', function(evt){
-
-// })
+rstElement.addEventListener('click', function(evt){
+  lisArr.forEach(element => element.remove());
+})
 
 ulElement.addEventListener('click', function(evt){
   evt.target.parentNode.removeChild(evt.target)
+  let idx = lisArr.indexOf(evt.target)
+  lisArr.splice(idx, 1)
+  // console.log('whatver', lisArr), testing new array
 })
+
